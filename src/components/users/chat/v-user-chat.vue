1caab7981090887d0694a9f060c4582b512db50b
AlexKosen <121030488+AlexKosen@users.noreply.github.com>
2023-04-20 21:54:47 +0300

    Add SET_MESSAGE_TO_CHAT

--- src/components/users/chat/v-user-chat.vue
+++ src/components/users/chat/v-user-chat.vue
@@ -1,6 +1,6 @@
 <script>
 import vMessageVue from './v-message.vue';
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
 export default {
     components: {
@@ -16,20 +16,40 @@ export default {
         //     type: Array,
         //     default: () => []
         // },
-        // user: {
-        //   type: Object,
-        //   default: () => {}  
-        // }
+        user: {
+          type: Object,
+          default: () => {}  
+        }
     },
     computed: {
         ...mapState([
             'currentChat'
+        ]),
+        ...mapActions([
+            'SET_MESSAGE_TO_CHAT'
         ])
     },
 
     methods: {
         sendMessage() {
-
+            let user = {...this.currentChat}
+            let chat = {
+                id: this.currentChat.chat.length + 1,
+                time: new Date().toLocaleTimeString('en-US', {
+                    hour12: false,
+                    hour: 'numeric',
+                    minute: 'numeric'
+                }),
+                text: this.textValue,
+                type: 'own'
+            }
+            user.chat.push(chat)
+            console.log(user.id)
+            console.log(user)
+            this.SET_MESSAGE_TO_CHAT({userId: user.id, chat: user})
+            .then(()=> {
+                this.textValue = ''
+            })
         }
     },
 }
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,52 +1,57 @@
 import { createStore } from "vuex";
 import axios from "axios";
 
-export const store = createStore ({
-    state() {
-        return { 
-            contacts: [],
-            chats: [],
-            currentUserChat: {},
-            currentChat: {}
-        }
+export const store = createStore({
+  state() {
+    return {
+      contacts: [],
+      chats: [],
+      currentUserChat: {},
+      currentChat: {},
+    };
+  },
+  getters: {},
+  mutations: {
+    SET_CONTACTS_TO_STORE(state, contacts) {
+      state.contacts = contacts;
     },
-    getters: {},
-    mutations: {
-        SET_CONTACTS_TO_STORE(state, contacts) {
-            state.contacts = contacts
-        },
-        SET_CHATS_TO_STORE(state, chats) {
-            state.chats = chats
-        },
-        SET_USER_TO_HEAD(state, user) {
-            if(user) {
-                state.currentUserChat = user
-            } else {
-                state.currentUserChat = ''
-            }   
-        },
-        SET_TO_CURRENT_CHAT(state, chat) {
-            state.currentChat = chat
-        }
+    SET_CHATS_TO_STORE(state, chats) {
+      state.chats = chats;
     },
-    actions: {
-        FETCH_CONTACTS({commit}) {
-            return axios.get('http://localhost:3000/contacts')
-            .then((contacts) => {
-                commit('SET_CONTACTS_TO_STORE', contacts.data)
-            })
-        },
-        FETCH_CHATS({commit}) {
-            return axios.get('http://localhost:3000/chats')
-            .then((chats) => {
-                commit('SET_CHATS_TO_STORE', chats.data)
-            })
-        },
-        SET_USER_TO_HEADER({commit}, user) {
-            commit('SET_USER_TO_HEAD', user)
-        },
-        ADD_CURRENT_CHAT({commit}, chat) {
-            commit('SET_TO_CURRENT_CHAT', chat)
-        }
-    }
-})
+    SET_USER_TO_HEAD(state, user) {
+      if (user) {
+        state.currentUserChat = user;
+      } else {
+        state.currentUserChat = "";
+      }
+    },
+    SET_TO_CURRENT_CHAT(state, chat) {
+      state.currentChat = chat;
+    },
+  },
+  actions: {
+    FETCH_CONTACTS({ commit }) {
+      return axios.get("http://localhost:3000/contacts").then((contacts) => {
+        commit("SET_CONTACTS_TO_STORE", contacts.data);
+      });
+    },
+    FETCH_CHATS({ commit }) {
+      return axios.get("http://localhost:3000/chats").then((chats) => {
+        commit("SET_CHATS_TO_STORE", chats.data);
+      });
+    },
+    SET_USER_TO_HEADER({ commit }, user) {
+      commit("SET_USER_TO_HEAD", user);
+    },
+    ADD_CURRENT_CHAT({ commit }, chat) {
+      commit("SET_TO_CURRENT_CHAT", chat);
+    },
+    SET_MESSAGE_TO_CHAT({ commit }, { userId, chat }) {
+      return axios
+        .put("http://localhost:3000/chats" + userId, chat)
+        .then((response) => {
+          return response;
+        });
+    },
+  },
+});
